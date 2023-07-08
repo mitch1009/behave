1a7f3e2dc178b681a5e5f51cdcb371768165c139
Mitch Chanza <mitch.chanza@gmail.com>
2023-07-08 18:39:02 +0200

    fix: remove dist folder

--- dist/support/reporter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const allure_cucumberjs_1 = require("allure-cucumberjs");
-class Reporter extends allure_cucumberjs_1.CucumberJSAllureFormatter {
-    constructor(options) {
-        super(options, new allure_cucumberjs_1.AllureRuntime({ resultsDir: "./allure-results" }), {});
-    }
-}
-exports.default = Reporter;
